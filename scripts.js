1f807a7f32552c1a26be0a4e4f50d66b0a1e2d99
vicky-wyq <72635661+vicky-wyq@users.noreply.github.com>
2021-01-25 17:47:10 -0800

    architecture page updated

--- scripts.js
+++ scripts.js
@@ -1,26 +1,48 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
   //do work
-  var carouselItems = document.getElementsByClassName("carousel-item");
-
-  var selectedIndex = 0;
 
 
 
-  document.getElementsByClassName("next")[0].addEventListener("click", function() {
-    carouselItems[selectedIndex].classList.remove("selected")
-    if (selectedIndex === carouselItems.length - 1) {selectedIndex = 0;
-  } else {selectedIndex = selectedIndex+1;
+
+  function makeCarousel(carousel) {
+    var selectedIndex = 0
+    
+    var carouselItems = carousel.getElementsByClassName("carousel-item");
+    carousel.getElementsByClassName("next")[0].addEventListener("click", function() {
+      carouselItems[selectedIndex].classList.remove("selected")
+    
+      if (selectedIndex === 0) {
+        selectedIndex = carouselItems.length - 1;
+      } else {
+        selectedIndex = selectedIndex - 1;
+      }
+      carouselItems[selectedIndex].classList.add("selected")
+    })
+    
+    carousel.getElementsByClassName("prev")[0].addEventListener("click", function() {
+      carouselItems[selectedIndex].classList.remove("selected")
+    
+      if (selectedIndex === carouselItems.length - 1) {
+        selectedIndex = 0;
+      } else {
+        selectedIndex = selectedIndex + 1;
+      }
+      carouselItems[selectedIndex].classList.add("selected")
+    })
   }
-    carouselItems[selectedIndex].classList.add("selected")
-  });
+  
 
-  document.getElementsByClassName("prev")[0].addEventListener("click", function() {
-    carouselItems[selectedIndex].classList.remove("selected")
 
-    if (selectedIndex === 0) {selectedIndex = carouselItems.length - 1;
-  } else {selectedIndex = selectedIndex-1;
+
+
+  var carousels = document.getElementsByClassName("carousel")
+  var index = 0;
+  while (index < carousels.length) {
+    makeCarousel(carousels[index]);
+    index++
   }
-    carouselItems[selectedIndex].classList.add("selected")
 
-  });
+
+
+
 });
