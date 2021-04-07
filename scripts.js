3c700905def4fb9f7a991e3a1273547117987140
vicky-wyq <72635661+vicky-wyq@users.noreply.github.com>
2021-04-07 12:55:49 -0700

    2.0

--- scripts.js
+++ scripts.js
@@ -3,6 +3,36 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  AOS.init({
+
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 1000, // values from 0 to 3000, with step 50ms
+
+  });
+  //aos + 2 links on html 
+
+
   const showAnim = gsap.from('.main-tool-bar', { 
     yPercent: -100,
     paused: true,
@@ -16,7 +46,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
       self.direction === -1 ? showAnim.play() : showAnim.reverse()
     }
   });
-  //main-tool-bar
+  //main-tool-bar + 2 links on html
 
 
   window.onscroll = function() {myFunction()};
@@ -27,8 +57,59 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var scrolled = (winScroll / height) * 100;
     document.getElementById("myBar").style.width = scrolled + "%";
   }
+  //progress-bar 0 link on html
+  function animateFrom(elem, direction) {
+    direction = direction | 1;
+    
+    var x = 0,
+        y = direction * 100;
+    if(elem.classList.contains("gs_reveal_fromLeft")) {
+      x = -100;
+      y = 0;
+    } else if(elem.classList.contains("gs_reveal_fromRight")) {
+      x = 100;
+      y = 0;
+    }
+    gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
+      duration: 1.25, 
+      x: 0,
+      y: 0, 
+      autoAlpha: 1, 
+      ease: "expo", 
+      overwrite: "auto"
+    });
+  }
+  function hide(elem) {
+    gsap.set(elem, {autoAlpha: 0});
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    gsap.registerPlugin(ScrollTrigger);
+    
+    gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
+      hide(elem); // assure that the element is hidden when scrolled into view
+      
+      ScrollTrigger.create({
+        trigger: elem,
+        onEnter: function() { animateFrom(elem) }, 
+        onEnterBack: function() { animateFrom(elem, -1) },
+        onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
+      });
+    });
+  }); 
+  //body scroll + 2 links on html
 
-  //progress-bar
+
+
+
+
+
+
+
+
+
+
+
 
 
 
