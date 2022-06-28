1199ff800d23c29113cc0bca589b588c1fa49102
vicky-wyq <72635661+vicky-wyq@users.noreply.github.com>
2022-06-27 23:43:25 -0700

    Appointa update

--- project/scripts_project.js
+++ project/scripts_project.js
@@ -1,6 +1,45 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   //do work
+  console.log(CryptoJS)
 
+  const pass = document.getElementById("password");
+  const submit = document.querySelectorAll(".passwordArr")[0];
+  const msg = document.getElementById("pwIncorrect");
+  const w = 'U2FsdGVkX19oLUQ5jGSt/8/BznuKKmnoGgwpbQPYIbeXe3qedrMX9ePhdFn9BFIe';
+
+  // Get the input field
+  var input = document.getElementById("password");
+
+  // Execute a function when the user presses a key on the keyboard
+  input.addEventListener("keypress", function (event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      submit.click();
+    }
+  });
+  submit.addEventListener("click", () => {
+    let redirect;
+    try {
+      let decrypted = CryptoJS.AES.decrypt(w, pass.value);
+      redirect = CryptoJS.enc.Utf8.stringify(decrypted);
+    } catch (e) {
+      console.log(e);
+    }
+
+    if (redirect && redirect.includes("/xb")) {
+      window.location.href = redirect;
+      msg.style.display = "none";
+    } else {
+      // handle some error
+      msg.style.display = "block";
+    }
+  });
+
+
+  //password
 // Modal Setup
 var modal = document.getElementById('modal');
 
@@ -47,46 +86,7 @@ document.addEventListener('click', function (e) {
   });
 
 
-  console.log(CryptoJS)
 
-  const pass = document.getElementById("password");
-  const submit = document.querySelectorAll(".passwordArr")[0];
-  const msg = document.getElementById("pwIncorrect");
-  const w = 'U2FsdGVkX19oLUQ5jGSt/8/BznuKKmnoGgwpbQPYIbeXe3qedrMX9ePhdFn9BFIe';
-
-  // Get the input field
-  var input = document.getElementById("password");
-
-  // Execute a function when the user presses a key on the keyboard
-  input.addEventListener("keypress", function (event) {
-    // If the user presses the "Enter" key on the keyboard
-    if (event.key === "Enter") {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      // Trigger the button element with a click
-      submit.click();
-    }
-  });
-  submit.addEventListener("click", () => {
-    let redirect;
-    try {
-      let decrypted = CryptoJS.AES.decrypt(w, pass.value);
-      redirect = CryptoJS.enc.Utf8.stringify(decrypted);
-    } catch (e) {
-      console.log(e);
-    }
-
-    if (redirect && redirect.includes("/xb")) {
-      window.location.href = redirect;
-      msg.style.display = "none";
-    } else {
-      // handle some error
-      msg.style.display = "block";
-    }
-  });
-
-
-  //password
 
 
   window.addEventListener('DOMContentLoaded', () => {
