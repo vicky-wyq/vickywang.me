313a11fdfb59d34eebb51afc6c91097e37182b41
vicky-wyq <72635661+vicky-wyq@users.noreply.github.com>
2022-11-16 16:29:44 -0800

    header js update

--- scripts.js
+++ scripts.js
@@ -1,6 +1,52 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   //do work
 
+  var lastKnownScrollY = 0;
+  var currentScrollY = 0;
+  var ticking = false;
+  var idOfHeader = 'header';
+  var eleHeader = null;
+  const classes = {
+    pinned: 'header-pin',
+    unpinned: 'header-unpin',
+  };
+  function onScroll() {
+    currentScrollY = window.pageYOffset;
+    requestTick();
+  }
+  function requestTick() {
+    if (!ticking) {
+      requestAnimationFrame(update);
+    }
+    ticking = true;
+  }
+  function update() {
+    if (currentScrollY < lastKnownScrollY) {
+      pin();
+    } else if (currentScrollY > lastKnownScrollY) {
+      unpin();
+    }
+    lastKnownScrollY = currentScrollY;
+    ticking = false;
+  }
+  function pin() {
+    if (eleHeader.classList.contains(classes.unpinned)) {
+      eleHeader.classList.remove(classes.unpinned);
+      eleHeader.classList.add(classes.pinned);
+    }
+  }
+  function unpin() {
+    if (eleHeader.classList.contains(classes.pinned) || !eleHeader.classList.contains(classes.unpinned)) {
+      eleHeader.classList.remove(classes.pinned);
+      eleHeader.classList.add(classes.unpinned);
+    }
+  }
+  window.onload = function() {
+    eleHeader = document.getElementById(idOfHeader);
+    document.addEventListener('scroll', onScroll, false);
+  }
+  //header
+
 
   console.log(CryptoJS)
 
@@ -108,5 +154,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
   }
   mybutton.addEventListener('click', topFunction)
   // back to top 
+
+
+
 
 });
