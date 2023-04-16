6087e59aba1295f537981d13a7ad49497de89125
Vicky <72635661+vicky-wyq@users.noreply.github.com>
2023-04-15 20:48:01 -0700

    new nav

---
+++ navigation/scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  //do work
+
+/*
+ * slide-nav
+ * ES6 Vanilla.js navigation plugin to simple use on one-page websites.
+ * https://github.com/qmixi/slide-nav
+ *
+ * A project by Piotr Kumorek
+ * Released under the MIT license.
+*/
+
+window.slide = new SlideNav();
+
+
+});
