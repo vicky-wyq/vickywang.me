a7ae30d35319397ab4f1e61c560fbac6b536015d
Vicky <72635661+vicky-wyq@users.noreply.github.com>
2023-05-09 16:52:51 -0700

    Design system left nav

--- scripts_project.js
+++ scripts_project.js
@@ -29,25 +29,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
       captionText.innerHTML = img.alt;
     }
   });
-/* window.addEventListener('DOMContentLoaded', () => {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        const id = entry.target.getAttribute('id');
-        if (entry.intersectionRatio > 0) {
-          document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
-        } else {
-          document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
-        }
-      });
-    });
-
-    // Track all sections that have an `id` applied
-    document.querySelectorAll('section[id]').forEach((section) => {
-      observer.observe(section);
-    });
-
-  });  */
-
 
   window.addEventListener('DOMContentLoaded', () => {
     const observer = new IntersectionObserver(entries => {
@@ -70,12 +51,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
       observer.observe(section);
     });
   });
-  
-
-  
-
-
-
 
 
 
