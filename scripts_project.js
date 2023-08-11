38be14023dbfd1778383130faeed729fc0f223c2
Vicky <72635661+vicky-wyq@users.noreply.github.com>
2023-08-10 17:20:40 -0700

    icons

--- scripts_project.js
+++ scripts_project.js
@@ -1,5 +1,42 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   //do work
+
+
+  const copyButtonLabel = "Copy Code";
+
+  // use a class selector if available
+  let blocks = document.querySelectorAll("pre");
+  
+  blocks.forEach((block) => {
+    // only add button if browser supports Clipboard API
+    if (navigator.clipboard) {
+      let button = document.createElement("button");
+  
+      button.innerText = copyButtonLabel;
+      block.appendChild(button);
+  
+      button.addEventListener("click", async () => {
+        await copyCode(block, button);
+      });
+    }
+  });
+  
+  async function copyCode(block, button) {
+    let code = block.querySelector("code");
+    let text = code.innerText;
+  
+    await navigator.clipboard.writeText(text);
+  
+    // visual feedback that task is completed
+    button.innerText = "Code Copied";
+  
+    setTimeout(() => {
+      button.innerText = copyButtonLabel;
+    }, 700);
+  }
+  
+
+
   const gifImages = document.querySelectorAll('.gif-toggle');
 
   gifImages.forEach(gifImage => {
