36f5c488ef7eca0b1bc8cec52980ef95ada143fd
Vicky <72635661+vicky-wyq@users.noreply.github.com>
2024-09-10 16:27:01 -0700

    landing and b2b booking update

--- templates/scripts.js
+++ templates/scripts.js
@@ -1,5 +1,9 @@
 
 //
+
+
+
+
 class FibonacciSphere {
   #points;
 
@@ -322,6 +326,86 @@ function initializePassword() {
 
 document.addEventListener("DOMContentLoaded", function (event) {
   //do work
+
+
+  /* =================== number animation  ===================*/
+  class CountUp {
+    constructor(el) {
+      this.el = el;
+      this.setVars();
+      this.init();
+    }
+  
+    setVars() {
+      this.number = this.el.querySelectorAll("[data-countup-number]");
+      this.observerOptions = { root: null, rootMargin: "0px 0px", threshold: 0 };
+      this.observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          const end = parseFloat(
+            entry.target.dataset.countupNumber.replace(/,/g, "")
+          );
+          const decimals = this.countDecimals(end);
+          if (entry.isIntersecting) {
+            this.iterateValue(entry.target, end, decimals);
+          }
+        });
+      }, this.observerOptions);
+    }
+  
+    init() {
+      if (this.number.length > 0) {
+        this.number.forEach((el) => {
+          this.observer.observe(el);
+        });
+      }
+    }
+  
+    iterateValue(el, end, decimals) {
+      const start = 0;
+      const duration = 2500;
+      let startTimestamp = null;
+  
+      const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const elapsedPercent = (timestamp - startTimestamp) / duration;
+        const easedProgress = Math.min(this.easeOutQuint(elapsedPercent), 1);
+        let interimNumber = Math.abs(easedProgress * (end - start) + start);
+        el.innerHTML = this.formatNumber(interimNumber, decimals);
+        if (easedProgress < 1) {
+          window.requestAnimationFrame(step);
+        }
+      };
+  
+      window.requestAnimationFrame(step);
+    }
+  
+    easeOutQuint(x) {
+      return 1 - Math.pow(1 - x, 5);
+    }
+  
+    countDecimals(val) {
+      if (Math.floor(val) === val) return 0;
+      return val.toString().split(".")[1]?.length || 0;
+    }
+  
+    formatNumber(val, decimals) {
+      return val.toLocaleString("en-US", {
+        minimumFractionDigits: decimals,
+        maximumFractionDigits: decimals
+      });
+    }
+  }
+  
+  // Simplified version to attach instances for this demo
+  const dataModules = [...document.querySelectorAll('[data-module="countup"]')];
+  
+  dataModules.forEach((element) => {
+    new CountUp(element);
+  });
+  
+  /* =================== number animation  ===================*/
+
+
   /* =================== click show pw  ===================*/
   var cards = document.querySelectorAll('.img__wrap');
 
@@ -339,6 +423,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
   /* =================== click show pw  ===================*/
 
   /* =================== carousel intro  ===================*/
+
+
+
 
   //autoplay  
 
