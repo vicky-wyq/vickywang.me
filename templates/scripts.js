87ea7de55ff6a0cde872a35a0f9373d2df12b407
Vicky <72635661+vicky-wyq@users.noreply.github.com>
2025-04-28 21:23:15 -0700

    landing

--- templates/scripts.js
+++ templates/scripts.js
@@ -533,10 +533,10 @@ const accTriggers = document.querySelectorAll(".accTrigger");
 accTriggers.forEach((trigger) => {
   trigger.addEventListener("click", function () {
     const content = this.nextElementSibling;
-    content.classList.toggle("is-open");
-    this.classList.toggle("is-open");
+    content.classList.toggle("is-open-acc");
+    this.classList.toggle("is-open-acc");
 
-    if (content.classList.contains("is-open")) {
+    if (content.classList.contains("is-open-acc")) {
       content.style.maxHeight = content.scrollHeight + "px";
     } else {
       content.style.maxHeight = null;
