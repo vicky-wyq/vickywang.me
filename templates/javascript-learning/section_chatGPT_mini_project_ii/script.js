38be5624befb1570f4cc691a39d9c4f2565ab057
Vicky <72635661+vicky-wyq@users.noreply.github.com>
2025-06-03 22:41:59 -0700

    Character Counter

--- templates/javascript-learning/section_chatGPT_mini_project_ii/script.js
+++ templates/javascript-learning/section_chatGPT_mini_project_ii/script.js
@@ -1,1 +1,38 @@
 "use strict";
+/*
+Event = ?
+count user typed in char
+
+State = ?
+
+
+DOM affected = ?
+show how many char left
+
+Logic = ?
+max char
+count current char
+provide feedback for how many char left
+
+
+*/
+
+const textarea = document.querySelector("#textarea");
+const feedback = document.querySelector("#feedback");
+
+
+function count() {
+  let currentChar = textarea.value.trim().length;
+
+  feedback.innerText = `Current characters are ${currentChar}, you have ${100-currentChar} left`;
+
+  if(currentChar>100){
+    feedback.innerText = `You can not exceed 100 characters`;
+        feedback.style.color = "red";
+
+  }
+
+
+}
+
+textarea.addEventListener("input", count);
