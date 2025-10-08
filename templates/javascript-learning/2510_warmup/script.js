056f62c442b515eb3ca7d398039aeb470958979d
Vicky <72635661+vicky-wyq@users.noreply.github.com>
2025-10-07 22:29:06 -0700

    js learning

---
+++ templates/javascript-learning/2510_warmup/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function createCounter(step) {
+  let count = 0;
+
+  return function () {
+    count+= step;
+    console.log(count);
+  };
+}
+
+// const counter = createCounter();
+
+// counter(); // 1
+// counter(); // 2
+
+//============================================
+
+const byFive = createCounter(5);
+byFive(); // why here = 1? the 5 not an argument? why 5 not pass in?
+
+// byFive(); // 10
+
+// const byTwo = createCounter(2);
+// byTwo(); // 2
+// byTwo(); // 4
